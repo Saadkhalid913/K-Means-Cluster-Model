82dcbf1b6a0dd6d0040977023013ea91c4bcac58
Saadkhalid913 <43319921+Saadkhalid913@users.noreply.github.com>
2021-03-17 22:02:04 -0400

    Update KMeans.py

--- KMeans.py
+++ KMeans.py
@@ -4,6 +4,7 @@ from sklearn.datasets import make_blobs
 import pandas as pd
 from sklearn.preprocessing import Normalizer, MinMaxScaler
 from math import sqrt
+from sklearn.metrics import confusion_matrix, accuracy_score
 
 
 class KMeansModel():
@@ -20,6 +21,7 @@ class KMeansModel():
         return sqrt(np.sum((v1-v2)**2))
 
     def FindClosestCentre(self, v1):
+        # loops through all centres and returns the index of closest centre
         ClosestCentreDistance = float("inf")
         ClosestCentreIndex = None
         for i in range(self.Centres.shape[0]):
@@ -30,6 +32,9 @@ class KMeansModel():
         return ClosestCentreIndex
 
     def MapClosestCentre(self):
+        # This is done for each sample and creates a column vector where the 
+        # element at index i of the vector corresponds to the closest centre of
+        # the ith sample
         Vec = []
         for i in range(self.N_Samples):
             Vec.append(self.FindClosestCentre(self.X[i]))
@@ -37,26 +42,45 @@ class KMeansModel():
         self.Classes = np.array(Vec).reshape((-1, 1))
 
     def Normalize(self):
+        # Normalizing function to express values between 0 and 1 
         N = MinMaxScaler()
         self.X = N.fit_transform(self.X)
 
     def ChangeCentres(self):
-        for i in range(self.Centres.shape[0]):
+        # updates each centre to be the take on the mean position of each 
+        # sample which takes on the corresponding class of the centre
+        for i in range(self.Centres.shape[0]): # we loop through all the centres 
             self.Centres[i] = np.sum(
-                self.X[np.array(km.Classes == i).reshape(1, -1)[0]], axis=0) / len(self.Classes[self.Classes == i])
+                # We use boolean indexing to only take the rows which correspond to the ith centre (the one we are currently re-calculating) 
+                self.X[np.array(km.Classes == i).reshape(1, -1)[0]], axis=0) / len(self.Classes[self.Classes == i]) # we divide by the number of samples in the class 
+
+if __name__ == "__main__":
+    # Creating some testing data 
+    k=2
+    TrainingX, TrainingY = make_blobs(
+        n_samples=1000, n_features=10, cluster_std=4, centers=k)
+    km = KMeansModel(k, TrainingX)
+    
+    km.MapClosestCentre()
 
 
-TrainingX, TrainingY = make_blobs(
-    n_samples=100, n_features=2, cluster_std=0.3, centers=2)
-km = KMeansModel(2, TrainingX)
-km.MapClosestCentre()
+    for i in range(50):
+        km.ChangeCentres()
+    data = np.concatenate((km.X, km.Classes), axis=1)
+    km.MapClosestCentre()
+    
+    # only uncomment if n_features =2 
+    # plt.scatter(data[:, : -2], data[:, 1: -1], c="r")
+    # plt.scatter(km.Centres[:, [0]], km.Centres[:, [1]], c="b")
+    # plt.show()
+    # print(np.concatenate((km.X, km.Classes), axis=1))
 
-
-for i in range(50):
-    km.ChangeCentres()
-data = np.concatenate((km.X, km.Classes), axis=1)
-
-
-plt.scatter(data[:, : -2], data[:, 1: -1], c="r")
-plt.scatter(km.Centres[:, [0]], km.Centres[:, [1]], c="b")
-plt.show()
+    # this code either yields a very low or high accuracy in most cases, this is just
+    # becuase this is an unsupervised learning model and that the names of classes 
+    # are completely arbitrary. A better measure of accuracy the the confusion matrix, 
+    # which shows classes and their frequencies 
+    pred = km.Classes
+    act = TrainingY.reshape(-1,1)
+    print(np.concatenate((pred,act ), axis=1 ))
+    print(confusion_matrix(pred,act ))
+    print(accuracy_score(pred, act) *100,"%")
